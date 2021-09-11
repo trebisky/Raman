12d3b1c65ca7e0361b4b095f431d2cb6bcc29123
Tom Trebisky <tom@mmto.org>
2021-09-11 15:31:15 -0700

    Find spectra library

--- raman.py
+++ raman.py
@@ -14,10 +14,33 @@ datafile = "BM_APT3-2_780_Hi.rruff"
 
 # on Windows 10, os.name returns "nt".
 #   on Linux it returns "posix"
-# on Windows 10, sys.platform returns "xx".
+# on Windows 10, sys.platform returns "win32".
 #   on Linux it returns "linux"
-print ( os.name )
-print ( sys.platform )
+#print ( os.name )
+#print ( sys.platform )
+
+if ( sys.platform == "linux" ) :
+    spectra_lib_dir = os.path.expanduser ( "~/RamanLib" )
+else :
+    spectra_lib_dir = "\CrystalSleuth\SearchRecords\RamanLib"
+
+if not os.path.isdir ( spectra_lib_dir ) :
+    print ( "Cannot find spectra library:", spectra_lib_dir )
+    exit ()
+
+print ( "Spectra library in", spectra_lib_dir )
+
+for name in os.listdir(spectra_lib_dir) :
+    print ( name )
+
+#files = [name for name in os.listdir(spectra_lib_dir)]
+#files = [name for name in os.listdir(spectra_lib_dir) if os.path.isfile(name)]
+files = [name for name in os.listdir(spectra_lib_dir) if os.path.isfile(os.path.join(spectra_lib_dir, name))]
+print ( files )
+
+#num_spec = len([name for name in os.listdir(spectra_lib_dir) if os.path.isfile(name)])
+num_spec = len ( files )
+print ( num_spec, "files in library" )
 
 f = open ( datafile, 'r')
 data = np.genfromtxt(f, delimiter=',')
